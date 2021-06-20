bd2c93105ea68d0d7dd996c582f42de1ff6695ff
Sherin <sherin.navaz@gmail.com>
2021-06-20 09:31:44 +0400

    create custom user model

---
+++ comments/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+#User Manager for overriding default unique username and enable unique email instead
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None,**kwargs):
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+#custom User class
+class User(AbstractBaseUser, PermissionsMixin):
+
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True,null=True)
+    is_active = models.BooleanField(default=True)
+    username = models.CharField(max_length=100)
+    is_staff = models.BooleanField(
+
+            default=False,
+
+        )
+    is_active = models.BooleanField(
+
+        default=True,
+
+    )
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+
+    def get_full_name(self):
+        return self.first_name+self.last_name
